01b23b856bb53dc80553250eacec842a294dc627
LarisaNichitoaia <139768565+LarisaNichitoaia@users.noreply.github.com>
2023-07-25 10:47:27 +0300

    Resolved bug

--- src/main/java/ro/msg/learning/shop/service/OrderService.java
+++ src/main/java/ro/msg/learning/shop/service/OrderService.java
@@ -7,6 +7,10 @@ import ro.msg.learning.shop.dto.ProvidedOrderDto;
 import ro.msg.learning.shop.repository.OrderRepository;
 import ro.msg.learning.shop.strategies.Strategy;
 
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import java.util.UUID;
+
 @RequiredArgsConstructor
 @Service
 public class OrderService {
@@ -15,9 +19,12 @@ public class OrderService {
     private final Strategy locationStrategy;
 
     public Order createOrder(ProvidedOrderDto orderToCreate) {
+        String dateString = orderToCreate.getCreatedAt();
+        DateTimeFormatter inputFormatter = DateTimeFormatter.ofPattern("yyyy-MM-dd'T'HH:mm:ss");
+        LocalDateTime parsedDate = LocalDateTime.parse(dateString, inputFormatter);
         locationStrategy.findLocationAndUpdateStock(orderToCreate.getProducts());
-        Customer customer = customerService.getCustomerById(orderToCreate.getCustomerId());
-        Order order = Order.builder().customer(customer).createdAt(orderToCreate.getCreatedAt())
+        Customer customer = customerService.getCustomerById(UUID.fromString(orderToCreate.getCustomerId()));
+        Order order = Order.builder().customer(customer).createdAt(parsedDate)
                 .addressCountry(orderToCreate.getAddressCountry()).addressCity(orderToCreate.getAddressCity())
                 .addressCounty(orderToCreate.getAddressCounty()).addressStreet(orderToCreate.getAddressStreet()).build();
         return orderRepository.save(order);
